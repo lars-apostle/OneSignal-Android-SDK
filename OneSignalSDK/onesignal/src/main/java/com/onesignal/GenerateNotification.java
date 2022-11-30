c04a9f5fa63f9dd8afe3953538bde4277a623881
lars_apostle <lars.hunze@apostle.nl>
2022-11-30 11:08:42 +0100

    Set correct flags

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/GenerateNotification.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/GenerateNotification.java
@@ -186,8 +186,8 @@ class GenerateNotification {
 
    private static PendingIntent getNewActionPendingIntent(int requestCode, Intent intent) {
       if (openerIsBroadcast)
-         return PendingIntent.getBroadcast(currentContext, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-      return PendingIntent.getActivity(currentContext, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+         return PendingIntent.getBroadcast(currentContext, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+      return PendingIntent.getActivity(currentContext, requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
    }
 
    private static Intent getNewBaseIntent(int notificationId) {
